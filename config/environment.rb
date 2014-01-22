0d95c605835feca15b495736d0bd6a03ae96da3d
Kevin McClure <kevin@KM-OLIN-LAPTOP.(none)>
2014-01-21 20:35:24 -0600

    got the thing working, simply execute the command: ruby parts_server_control.rb run

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,25 @@
 # Fill in parameters for development/local database and e-mail account here. Changes for prod should be made
 # in the Rakefile, as this file is overwritten on deploy.
 
+# ALL OF THE OLD STUFF
+# PORT = 9000
+# DB_HOST = "localhost"
+# DB_USER = "team254"
+# DB_PASSWORD = "correcthorsebatterystaple"
+# DB_DATABASE = "cheesy_parts"
+# URL = "http://parts.team254.com"
+# GMAIL_USER = "cheesyparts@gmail.com"
+# GMAIL_PASSWORD = "correcthorsebatterystaple"
+# WORDPRESS_AUTH_URL = "http://www.team254.com/auth/"
+# LOGOUT_URL = "http://team254.com"
+
 PORT = 9000
 DB_HOST = "localhost"
-DB_USER = "team254"
-DB_PASSWORD = "correcthorsebatterystaple"
-DB_DATABASE = "cheesy_parts"
-URL = "http://parts.team254.com"
-GMAIL_USER = "cheesyparts@gmail.com"
-GMAIL_PASSWORD = "correcthorsebatterystaple"
+DB_USER = "root"
+DB_PASSWORD = "Gkikh9tc"
+DB_DATABASE = "phoenix_parts"
+URL = "file:///home/kevin/Documents/Baja/PartsTracker/Website/index.html"
+GMAIL_USER = "phoenixracingfabrication@gmail.com"
+GMAIL_PASSWORD = "correcthorsebatterystaples"
 WORDPRESS_AUTH_URL = "http://www.team254.com/auth/"
 LOGOUT_URL = "http://team254.com"
