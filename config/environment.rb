7043bdec158237968b48edcb5214f5ee0d990224
patrick <patrickjvarin@gmail.com>
2014-02-05 22:18:28 -0500

    modified port environment variable

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 # in the Rakefile, as this file is overwritten on deploy.
 
 
-PORT = $PORT
+PORT = ENV["PORT"]
 DB_HOST = "localhost"
 DB_HOST = "us-cdbr-east-05.cleardb.net"#/heroku_ef2aea1c34afa4b?reconnect=true
 DB_USER = "phoenix"
@@ -11,6 +11,7 @@ DB_PASSWORD = "00a961fd"
 DB_DATABASE = "phoenix_parts"
 DB_DATABASE = "heroku_ef2aea1c34afa4bher"
 URL = "0.0.0.0:9000"
+URL = "phoenix_parts.herokuapp.com"
 GMAIL_USER = "phoenixracingfabrication@gmail.com"
 GMAIL_PASSWORD = "correcthorsebatterystaples"
 WORDPRESS_AUTH_URL = ""
