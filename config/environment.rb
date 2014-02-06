d2583bacbd7c3e783cd27c6d99bdbb05b5e34a7e
patrick <patrickjvarin@gmail.com>
2014-02-05 22:42:17 -0500

    think I found one

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ DB_HOST = "us-cdbr-east-05.cleardb.net"#/heroku_ef2aea1c34afa4b?reconnect=true
 DB_USER = "bb0c958fee3cfc"
 DB_PASSWORD = "00a961fd"
 # DB_DATABASE = "phoenix_parts"
-DB_DATABASE = "heroku_ef2aea1c34afa4bher"
+DB_DATABASE = "heroku_ef2aea1c34afa4b"#her
 # URL = "0.0.0.0:9000"
 URL = "phoenix_parts.herokuapp.com"
 GMAIL_USER = "phoenixracingfabrication@gmail.com"
